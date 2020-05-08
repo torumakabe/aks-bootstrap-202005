7a576b13b13a6fc56fbc17f88b638a8d39dd8ba0
ToruMakabe <tomakabe@microsoft.com>
2020-05-09 08:48:07 +0900

    add flag for flux

--- src/scripts/setenv-sample.sh
+++ src/scripts/setenv-sample.sh
@@ -8,6 +8,7 @@ export TF_VAR_aks_cluster_rg="rg-aks-awesome-cluster"
 export TF_VAR_aks_cluster_location="japaneast"
 export TF_VAR_la_workspace_name=""
 export TF_VAR_la_workspace_rg=""
+export TF_VAR_enable_flux=true
 export TF_VAR_git_authuser="${GITHUB_ACTOR}"
 export TF_VAR_git_authkey=""
 export TF_VAR_git_fluxrepo="flux-demo"
